7cc25015c9dba6cb90cdeb0e1014d31b966db2b4
Andrew Hunter <andrew@aehunter.net>
2007-11-24 07:42:25 +0000

    Changed _append to iterate over the file instead of operating on one big string. Added test for memlock.py

--- memlock.py
+++ memlock.py
@@ -12,13 +12,15 @@ import re
 
 find_memlock = re.compile('@audio - memlock \d*')
 limits_conf = open('/etc/security/limits.conf', 'r+')
+limits_conf_read = open('/etc/security/limits.conf', 'r+')
 limits_conf_append = open('/etc/security/limits.conf', 'a+')
-oldlines = limits_conf.read()
+oldlines = limits_conf_read.read()
 
 def ch_memlock(new_memlock):
   line_replacement = '@audio - memlock ' + new_memlock
   memlock_check = find_memlock.search(oldlines) #determine if an audio memlock already exists
   if memlock_check:
+    print 'memlock_check is true'
     _update(line_replacement)
     limits_conf.close()
   else:
@@ -27,9 +29,16 @@ def ch_memlock(new_memlock):
     limits_conf_append.close()
 
 def _update(line_replacement):
-  newlines = find_memlock.sub(line_replacement, oldlines)
+  newlines = []
+  for item in limits_conf:
+    line_memlock = find_memlock.search(item) #Is the current line the one we want?
+    if line_memlock:
+      newlines.append(find_memlock.sub(line_replacement, item))
+    else:
+      newlines.append(item)
+
   limits_conf.seek(0)
-  limits_conf.write(newlines)
+  limits_conf.writelines(newlines)
 
 def _append(line_replacement): #FIXME: Does not respect #End of file.
   limits_conf_append.seek(-14, 2)
---
+++ memlock_test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import memlock
+
+memlock.ch_memlock('5120010')
