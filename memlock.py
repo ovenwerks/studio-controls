93fcdd322adefce3b822ba9a48e6e1a9f3ed2a0f
Andrew Huntwe <rexbron@snow-vm>
2007-11-26 22:08:37 +0000

    Make memlock.py not break things.

--- memlock.py
+++ memlock.py
@@ -5,23 +5,18 @@
 #  Created by Andrew Hunter on 25/11/07.
 #
 # TODO: Add way to append the correct line if one does not exist.
+# TODO: Backup conf file before overwriting :P
 
 import re
 
-find_memlock = re.compile('@audio - memlock (\d{6})') #create the regex that we are looking for
+find_memlock = re.compile('@audio - memlock (\d{6})')
 
-line_replacement = '@audio - memlock ' #FIXME: Make the user input varriables
+def ch_memlock(new_memlock):
+  line_replacement = '@audio - memlock ' + new_memlock
+  limits_conf = open('/etc/security/limits.conf', 'r+')
+  oldlines = limits_conf.read()
+  newlines = find_memlock.sub(line_replacement, oldlines)
+  limits_conf.seek(0)
+  limits_conf.write(newlines)
+  limits_conf.close()
 
-new_memlock = '512000'
-
-limits_conf = '/etc/security/limits.conf'
-
-def ch_memlock(line_replacement, new_memlock, limits_conf):
-  for line in fileinput.FileInput(limit_conf, inplace=1):
-    try:
-        find_memlock.sub(line_replacement + new_memlock, line)
-    except:
-        print 'There has been a error with the string substution.'
-
-
-   
