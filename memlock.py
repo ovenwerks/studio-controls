9ffef5ed8fa868a81a11733a81cc94a4ddd952b5
Andrew Hunter <andrew@aehunter.net>
2007-11-28 18:34:34 +0000

    Added rm_memlock and refactored code

--- memlock.py
+++ memlock.py
@@ -4,9 +4,9 @@
 #
 #  Created by Andrew Hunter on 25/11/07.
 #
-# TODO: Add way to append the correct line if one does not exist.
 # TODO: Backup conf file before overwriting :P
 # TODO: Come up with better varriable names
+# TODO: Find a way to read files multiple times.
 
 import re
 
@@ -16,15 +16,16 @@ limits_conf_read = open('/etc/security/limits.conf', 'r')
 oldlines = limits_conf_read.read()
 
 def ch_memlock(new_memlock):
+  """
+  Adds or updates the @audio - memlock option in /etc/security/limits.conf
+  """
   line_replacement = '@audio - memlock ' + new_memlock
   memlock_check = find_memlock.search(oldlines) #determine if an audio memlock already exists
   if memlock_check:
     print 'memlock_check is true'
     _update(line_replacement)
-    limits_conf.close()
   else:
     _append(line_replacement)
-    limits_conf.close()
 
 def _update(line_replacement):
   newlines = []
@@ -34,13 +35,27 @@ def _update(line_replacement):
       newlines.append(find_memlock.sub(line_replacement, item))
     else:
       newlines.append(item)
+  _seek_write(newlines)
 
+def _seek_write(open_list):
   limits_conf.seek(0)
-  limits_conf.writelines(newlines)
+  limits_conf.writelines(open_list)
+  limits_conf.truncate()
+  limits_conf.flush()
 
 def _append(line_replacement):
   append_list = limits_conf.readlines()
   append_list[-1] = line_replacement + '\n\n#End of file' #Assumes that #End of file is the last line
-  limits_conf.seek(0)
-  limits_conf.writelines(append_list)
+  _seek_write(append_list)
+
+def rm_memlock():
+  memlock_check = find_memlock.search(oldlines) #determine if an audio memlock already exists
+  if memlock_check:
+    memlock_string = '\n' + memlock_check.group() + '\n'
+    rm_list = limits_conf.readlines()
+    rm_list.remove(memlock_string)
+    _seek_write(rm_list)
+  else:
+    print "No memlock to remove"
+  
 
---
+++ rm_test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import memlock
+
+memlock.rm_memlock()
