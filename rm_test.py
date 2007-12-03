e106130c85c73a0a16bd1b84964142f06c2e66f0
Luis de Bethencourt Guimera <bethencourt@gmail.com>
2007-12-03 21:58:16 +0100

    gui changed to glade

--- rm_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-#Example for removing the memlock line from limits.conf
-
-import changesettings
-
-memlock = changesettings.ChangeSettings('/etc/security/limits.conf', '@audio - memlock (\d*)', '@audio - memlock')
-
-memlock.rm_setting()
