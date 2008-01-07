3bc150c6b8b17ec43ca1fdaebfb322aeabb9edc9
Andrew Hunter <andrew@aehunter.net>
2008-01-07 17:17:51 -0500

    Added initial attempt at adding distutils support (Non-funtional).

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+Setup(name='Ubuntu Studio Controls',
+  version='0.1',
+  author='Ubuntu Studio Development Team',
+  author_email='ubuntu-studio-devel@lists.ubuntu.com',
+  url='https://wiki.ubuntu.com/UbuntuStudio/SettingsApp',)
+
