b3ec65b9d8f91de1048d2579d34c4eedd420f1a8
Andrew Hunter <andrew@aehunter.net>
2008-01-08 18:39:22 -0500

    I loves me my distutils tweaks.

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(name = 'ubuntustudio-controls',
   author = 'Ubuntu Studio Development Team',
   author_email = 'ubuntu-studio-devel@lists.ubuntu.com',
   url = 'https://wiki.ubuntu.com/UbuntuStudio/SettingsApp',
+  license = 'GPL v2',
   package_dir = {'':'src'},
   py_modules = ['changesettings', 'memtotal_info'],
-  scripts = ['src/ubuntustudio-controls.py'],
-  data_files = [('share/ubuntustudio-controls/glade', ['src/gui.glade', 'src/ubuntustudio-controls.svg'])],
+  scripts = ['src/ubuntustudio-controls'],
+  data_files = [('share/ubuntustudio-controls', ['src/gui.glade', 'src/ubuntustudio-controls.svg'])],
   classifiers = ['License :: OSI Approved :: GNU General Public License (GPL)'])
 
