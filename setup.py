6171090dcc8ebf839580ed7f40524206779bdee3
Andrew Hunter <andrew@aehunter.net>
2008-01-07 19:40:15 -0500

    More clean ups.

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 
 from distutils.core import setup
 
-Setup(name='Ubuntu Studio Controls',
-  version='0.1',
-  author='Ubuntu Studio Development Team',
-  author_email='ubuntu-studio-devel@lists.ubuntu.com',
-  url='https://wiki.ubuntu.com/UbuntuStudio/SettingsApp',)
+setup(name = 'ubuntustudio-controls',
+  version = '0.1',
+  author = 'Ubuntu Studio Development Team',
+  author_email = 'ubuntu-studio-devel@lists.ubuntu.com',
+  url = 'https://wiki.ubuntu.com/UbuntuStudio/SettingsApp',
+  package_dir = {'':'src'},
+  py_modules = ['changesettings', 'memtotal_info'],
+  scripts = ['main'],)
 
