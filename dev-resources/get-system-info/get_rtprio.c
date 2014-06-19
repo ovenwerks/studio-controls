b314630f02b2662d4ecbe8ffa4f3267a97e7684f
Kaj Ailomaa <zequence@mousike.me>
2014-06-19 13:08:38 +0200

    added some resources that can be used for developing the new ubuntustudio-controls

---
+++ dev-resources/get-system-info/get_rtprio.c
@@ -0,0 +1,12 @@
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include<stdio.h>
+
+int main ()
+{
+	struct rlimit rl;
+	getrlimit (RLIMIT_RTPRIO, &rl);
+	printf("%lld\n", (long long int)rl.rlim_cur);
+	return 0;
+}
