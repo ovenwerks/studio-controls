5a796abdd224d97295745baf4388fececcff8dd4
Len Ovens <len@ovenwerks.net>
2020-06-21 23:31:00 -0700

    Phones switching started

---
+++ etc/acpi/studio.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# send signal to autojack
+echo Sending ${1} signal
+/usr/bin/dbus-send --system --type=signal / org.studio.control.event.${1}_signal
+
+# Note: in most cases -controls can automatoically switch from speakers
+#		to phones and back via alsa controling. In some cases pin
+#		function needs to be changed as root. This would be the place to
+#		add that below.
+
